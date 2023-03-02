7d155f2cd83d5595d306946096d2dbc580b6df54
Sunmin Eom <Annie@Hyeonsangs-MacBook-Pro.local>
2023-03-02 08:06:42 -0800

    model dictionary update

--- models/__init__.py
+++ models/__init__.py
@@ -13,7 +13,19 @@ vae_models = {
     'MMD_VAE':mmd_vae.MMD_VAE,
     'MMD_CVAE':conditional_mmd_vae.MMD_CVAE,
     'VQ_VAE':vq_vae.VQVAE,
-    'MLP':mlp.MLP
+    'MLP':mlp.MLP,
+    'CNN_VAE':vae_cnn.VAE,
+    'CNN_CVAE':conditional_vae_cnn.CVAE,
+    'CNN_MMD_VAE':mmd_vae_cnn.MMD_VAE,
+    'CNN_MMD_CVAE':conditional_mmd_vae_cnn.MMD_CVAE,
+    'CNN_VQ_VAE':vq_vae_cnn.VQVAE,
+    'CNN_MLP':mlp_cnn.MLP,
+    'RNN_VAE':vae_rnn.VAE,
+    'RNN_CVAE':conditional_vae_rnn.CVAE,
+    'RNN_MMD_VAE':mmd_vae_rnn.MMD_VAE,
+    'RNN_MMD_CVAE':conditional_mmd_vae_rnn.MMD_CVAE,
+    'RNN_VQ_VAE':vq_vae_rnn.VQVAE,
+    'RNN_MLP':mlp_rnn.MLP
     }
 
 
