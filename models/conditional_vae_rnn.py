0317d476e28fb22e7fa7c12f5a8529bbe1264b7a
Sunmin Eom <Annie@Hyeonsangs-MacBook-Pro.local>
2023-06-13 08:08:45 -0700

    debug

--- models/conditional_vae_rnn.py
+++ models/conditional_vae_rnn.py
@@ -64,7 +64,6 @@ class CVAE(nn.Module):
         return torch.cat((y,self.decoder(z)),1)
 
     def loss_function(self, recon_x, x, **kwargs):
-        print(x.shape)
         recon_loss = F.cross_entropy(recon_x.transpose(1,2), x.transpose(1,2), reduction='none')
         # change contribution weight of ts to loss - for some weird reason the model does not train with mean readuction
         #recon_loss = torch.mean((recon_loss[:,:kwargs.get('ts_len',13)] * kwargs.get('ts_weight',1))) + torch.mean(recon_loss[:,kwargs.get('ts_len',13):])
