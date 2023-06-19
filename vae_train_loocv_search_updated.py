7e5a2b5dfee67c08065ef43c7b03de63f716fcee
Sunmin Eom <Annie@Hyeonsangs-MacBook-Pro.local>
2023-06-19 00:30:00 -0700

    batch size

--- vae_train_loocv_search_updated.py
+++ vae_train_loocv_search_updated.py
@@ -22,34 +22,36 @@ if __name__ == '__main__':
     parser.add_argument('-m','--model_type', nargs='?', default='CVAE', type=str, help='default = %(default)s; select the type of VAE model to use; options: VAE, CVAE, SVAE, MMD_VAE, VQ_VAE')
     parser.add_argument('--specific_libs', nargs='*', default='all', type=str, help='default = %(default)s; leave one out testing only for specific libraries, seperate names space')
     parser.add_argument('-l','--layer_sizes', nargs='*', default=[64,32], type=int, help='default = %(default)s; the hidden layer dimensions in the model', dest='layer_sizes')
+    parser.add_argument('-b','--batch_size', nargs='?', default=128, type=int, help='default = %(default)s; the number of samples in each processing batch', dest='batch_size')
+
     args = parser.parse_args()
     # for es in [10, 40]:
     #     for bs in [64, 128, 512]:
     #         for lr in [1e-3, 1e-4, 1e-5]:
     #             for las in [2, 4, 6]:
-    for bs in [64, 32]:
-        for lr in [1e-3, 1e-4, 1e-5]:
-            lys = args.layer_sizes
-            lys = ' '.join(str(x) for x in lys)
-            libs = ' '.join(args.specific_libs)
-            es = 40
-            las = 2
-            model_folder = os.path.join(args.outfolder, f'{es}-{bs}-{lr}-{las}-{lys.replace(" ", "_")}-{libs.replace(" ", "_")}-1')
-            if os.path.exists(model_folder):
-                pred_path = os.path.join(model_folder, 'prediction_hamming.csv')
-                if os.path.exists(pred_path):
-                    continue
-                else:
-                    shutil.rmtree(model_folder)
-            settings = f'vae --outfolder {model_folder} \
-                    --input_data {args.input_data} \
-                    --epochs {es}\
-                    --batch_size {bs}\
-                    --latent_size {las}\
-                    --layer_sizes {lys}\
-                    --model_type {args.model_type}\
-                    --learning_rate {lr}\
-                    --specific_libs {libs}'
-            print(settings.split())
-            sys.argv = settings.split()
-            full_main()
+    for lr in [1e-4, 1e-5, 1e-3]:
+        lys = args.layer_sizes
+        lys = ' '.join(str(x) for x in lys)
+        libs = ' '.join(args.specific_libs)
+        bs = args.batch_size
+        es = 40
+        las = 2
+        model_folder = os.path.join(args.outfolder, f'{es}-{bs}-{lr}-{las}-{lys.replace(" ", "_")}-{libs.replace(" ", "_")}-1')
+        if os.path.exists(model_folder):
+            pred_path = os.path.join(model_folder, 'prediction_hamming.csv')
+            if os.path.exists(pred_path):
+                continue
+            else:
+                shutil.rmtree(model_folder)
+        settings = f'vae --outfolder {model_folder} \
+                --input_data {args.input_data} \
+                --epochs {es}\
+                --batch_size {bs}\
+                --latent_size {las}\
+                --layer_sizes {lys}\
+                --model_type {args.model_type}\
+                --learning_rate {lr}\
+                --specific_libs {libs}'
+        print(settings.split())
+        sys.argv = settings.split()
+        full_main()
