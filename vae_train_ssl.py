6bb4cb2201609f56622bdfbbba1f3da9fb4aecd8
Sunmin Eom <Annie@Hyeonsangs-MacBook-Pro.local>
2023-06-22 10:04:55 -0700

    compare model architectures to identify source of error

--- vae_train_ssl.py
+++ vae_train_ssl.py
@@ -66,7 +66,7 @@ def pre_train():
     latent_size=args.latent_size,
     ts_len=0,
     layer_kwargs={'batchnorm':args.batch_norm, 'dropout_p':args.dropout_p})
-
+    print(model)
     model, losses = training.model_training(model, yx_oh, yx_oh, epochs=args.epochs, batch_size=args.batch_size, loss_kwargs={'beta':args.beta}, optimizer_kwargs={'lr':args.learning_rate})
 
     # save model
