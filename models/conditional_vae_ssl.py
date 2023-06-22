a73a6efd30656dcaf3528869f4a9bb49b8c38392
Sunmin Eom <Annie@Hyeonsangs-MacBook-Pro.local>
2023-06-22 10:33:53 -0700

    debug

--- models/conditional_vae_ssl.py
+++ models/conditional_vae_ssl.py
@@ -44,6 +44,7 @@ class CVAE(nn.Module):
         super(CVAE, self).__init__()
         self.input_shape = (input_shape[0]-ts_len, input_shape[1])
         self.ts_len = ts_len
+        self.padding = 0
         if ts_len == 0:
             ts_len = 13
             self.padding = ts_len
