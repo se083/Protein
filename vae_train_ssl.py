e00969007ced80550ea166918553d2e1911cef12
se083 <Annie@Hyeonsangs-MacBook-Pro.local>
2023-06-27 07:45:50 -0700

    debug

--- vae_train_ssl.py
+++ vae_train_ssl.py
@@ -67,7 +67,7 @@ def pre_train():
     latent_size=args.latent_size,
     ts_len=0,
     layer_kwargs={'dropout_p':args.dropout_p},
-    num_layers=arg.num_layers)
+    num_layers=args.num_layers)
     print(model)
     model, losses = training.model_training(model, yx_oh, yx_oh, epochs=args.epochs, batch_size=args.batch_size, loss_kwargs={'beta':args.beta}, optimizer_kwargs={'lr':args.learning_rate})
 
