502ddc572cc3ab658df8d9c543b56c5f2b368705
se083 <Annie@Hyeonsangs-MacBook-Pro.local>
2024-03-13 09:17:57 -0700

    removing duplicates to a threshold

--- load_data.py
+++ load_data.py
@@ -9,6 +9,14 @@ def load_Rec_TS(file = 'example_input/RecGen-training-data.csv', nreads = 1000, 
     # get input
     combdf = pd.read_csv(file)
 
+    max_dups = 5
+    combdf['dups'] = 1
+    cdf = combdf.groupby(['trained_target_site', 'Sequence', 'target_sequence']).count().reset_index()
+    cdf.to_csv('/content/drive/MyDrive/Data/Protein/combdf_count.csv')
+    cdf['dups'] = cdf['dups'].apply(lambda x: min(x,max_dups))
+    combdf = cdf.loc[cdf.index.repeat(cdf['dups'])]
+
+
     # calculating minimum number of sequences per library
     mdf = combdf.groupby('trained_target_site').count()["target_sequence"].min()
     print(mdf)
--- models/conditional_vae_cnn.py
+++ models/conditional_vae_cnn.py
@@ -75,8 +75,8 @@ class CVAE(nn.Module):
     def loss_function(self, recon_x, x, **kwargs):
         # recon_x = recon_x[:, max(self.padding, self.ts_len):]
         # x = x[:, self.ts_len:]
-        # recon_loss = F.cross_entropy(recon_x.transpose(1,2), x.transpose(1,2), reduction='none')
-        recon_loss = F.binary_cross_entropy(recon_x, x, reduction='none')
+        recon_loss = F.cross_entropy(recon_x.transpose(1,2), x.transpose(1,2), reduction='none')
+        # recon_loss = F.binary_cross_entropy(recon_x, x, reduction='none')
         # change contribution weight of ts to loss - for some weird reason the model does not train with mean readuction
         # recon_loss = torch.mean((recon_loss[:,:kwargs.get('ts_len',13)] * kwargs.get('ts_weight',1))) + torch.mean(recon_loss[:,kwargs.get('ts_len',13):])
         # if 'ts_len' in kwargs and kwargs['ts_len']>0:
