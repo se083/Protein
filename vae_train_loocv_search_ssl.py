5512aaff365c1edd1a33e0a13dd3a9359d8ae849
se083 <Annie@Hyeonsangs-MacBook-Pro.local>
2024-07-01 00:59:02 -0700

    change script name

--- vae_train_loocv_search_ssl.py
+++ vae_train_loocv_search_ssl.py
@@ -10,7 +10,7 @@ import pandas as pd
 from collections import defaultdict
 import subprocess
 import argparse
-from vae_train_val import full_main
+from vae_train_loocv import full_main
 from vae_train_ssl import pre_train
 
 import sys
@@ -29,47 +29,51 @@ if __name__ == '__main__':
     parser.add_argument('-l','--layer_sizes', nargs='*', default=[64,32], type=int, help='default = %(default)s; the hidden layer dimensions in the model', dest='layer_sizes')
     parser.add_argument('-lr','--learning_rate', nargs='?', default=0.001, type=float, help='default = %(default)s; the rate of learning, higher means faster learning, but can lead to less accuracy', dest='learning_rate')
     parser.add_argument('-nl','--num_layers', nargs='?', default=1, type=int, help='default = %(default)s; the number of LSTM layers', dest='num_layers')
+    parser.add_argument('-a','--beta', nargs='?', default=1, type=float, help='default = %(default)s; the final weight on the KL-Divergence', dest='beta')
+    parser.add_argument('-dup','--maximum_duplicates', nargs='?', default=1, type=int, help='default = %(default)s; the multiplyer applied to the reconstruction loss of the target site', dest='ts_weight')
+    parser.add_argument('-prop','--maximum_proportion', nargs='?', default=1, type=float, help='default = %(default)s; the multiplyer applied to the reconstruction loss of the target site', dest='ts_weight')
 
     args = parser.parse_args()
-    for es in [10, 20, 40]:
-        for lr in [1e-3, 1e-4, 1e-5]:
-            for bs in [32, 64, 128]:
-                lys = args.layer_sizes
-                lys = ' '.join(str(x) for x in lys)
-                libs = ' '.join(args.specific_libs)
-                las = 2
-                nl = args.num_layers
-                model_folder = os.path.join(args.outfolder, f'{es}-{bs}-{lr}-{las}-{lys.replace(" ", "_")}-{libs.replace(" ", "_")}-{nl}')
-                if os.path.exists(model_folder):
-                    pred_path = os.path.join(model_folder, 'prediction_hamming.csv')
-                    if os.path.exists(pred_path):
-                        continue
-                    else:
-                        shutil.rmtree(model_folder)
-                settings = f'vae --outfolder {model_folder} \
-                        --input_data {args.pre_input_data} \
-                        --epochs {es}\
-                        --batch_size {bs}\
-                        --model_type {args.pre_train_model_type}\
-                        --learning_rate {lr}\
-                        -l {lys}\
-                        -nl {nl}'
-                print(settings.split())
-                sys.argv = settings.split()
-                pre_train()
-                pre_model = model_folder + '/' + args.pre_train_model_type + '_weights.pt'
-                model_folder = os.path.join(model_folder, f'40-128-0.001-{libs.replace(" ", "_")}')
-                settings = f'vae --outfolder {model_folder} \
-                        --input_data {args.input_data} \
-                        --epochs 40\
-                        --batch_size 128\
-                        --model_type {args.fine_tune_model_type}\
-                        --learning_rate 0.001\
-                        --specific_libs {args.specific_libs}\
-                        --pre_model {pre_model}\
-                        -l {lys}\
-                        -nl {nl}'
-                print(settings.split())
-                sys.argv = settings.split()
-                full_main()
+    es=40
+    for lr in [1e-3, 1e-4, 1e-5]:
+        for bs in [32, 64, 128]:
+            lys = args.layer_sizes
+            lys = ' '.join(str(x) for x in lys)
+            las = 2
+            nl = args.num_layers
+            model_folder = os.path.join(args.outfolder, f'{es}-{bs}-{lr}-{las}-{lys.replace(" ", "_")}-{nl}')
+            if os.path.exists(model_folder):
+                fine_tune_folder = os.path.join(model_folder, f'{args.epochs}-{args.batch_size}-{args.learning_rate}')
+                pred_path = os.path.join(fine_tune_folder, 'prediction_hamming.csv')
+                if os.path.exists(pred_path):
+                    continue
+                else:
+                    shutil.rmtree(model_folder)
+            settings = f'vae --outfolder {model_folder} \
+                    --input_data {args.pre_input_data} \
+                    --epochs {es}\
+                    --batch_size {bs}\
+                    --model_type {args.pre_train_model_type}\
+                    --learning_rate {lr}\
+                    -l {lys}\
+                    -nl {nl}\
+                    --beta {args.beta}'
+            print(settings.split())
+            sys.argv = settings.split()
+            pre_train()
+            pre_model = model_folder + '/' + args.pre_train_model_type + '_weights_0.pt'
+            model_folder = os.path.join(model_folder, f'{args.epochs}-{args.batch_size}-{args.learning_rate}')
+            settings = f'vae --outfolder {model_folder} \
+                    --input_data {args.input_data} \
+                    --epochs {args.epochs}\
+                    --batch_size {args.batch_size}\
+                    --model_type {args.fine_tune_model_type}\
+                    --learning_rate {args.learning_rate}\
+                    --pre_model {pre_model}\
+                    -l {lys}\
+                    -nl {nl}\
+                    --beta {args.beta}'
+            print(settings.split())
+            sys.argv = settings.split()
+            full_main()
 
